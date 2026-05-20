ea48917290f9a67ff8e0446dd505bf43e92f57af
Neruoal <manuellareogarcia@gmail.com>
2026-05-20 08:20:04 +0000

    NO HELLO

--- src/api/routes.py
+++ src/api/routes.py
@@ -41,14 +41,6 @@ def token_required(f):
     return decorated
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-    return jsonify(response_body), 200
-
-
 @api.route('/signup', methods=['POST'])
 def signup():
     body = request.get_json()
